91efcaa9636df9745954ccc833a3d0632e3f04c9
Yad <lolyer47@gmail.com>
2024-02-09 09:52:08 +0300

    Added random events

--- Assets/Scripts/DataScripts/GuiData.cs
+++ Assets/Scripts/DataScripts/GuiData.cs
@@ -25,6 +25,13 @@ public class GuiData : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _buy;
     [SerializeField] private TextMeshProUGUI _effect;
 
+    [SerializeField] private TextMeshProUGUI _eventName;
+    [SerializeField] private TextMeshProUGUI _eventDescription;
+    [SerializeField] private TextMeshProUGUI _eventOption1;
+    [SerializeField] private TextMeshProUGUI _eventOption2;
+    [SerializeField] private TextMeshProUGUI _eventEffect1;
+    [SerializeField] private TextMeshProUGUI _eventEffect2;
+
     public TextMeshProUGUI Resources 
     { 
         get => _resources; 
@@ -113,5 +120,41 @@ public class GuiData : MonoBehaviour
     {
         get => _effect;
         set => _effect = value;
+    }
+
+    public TextMeshProUGUI EventName
+    {
+        get => _eventName;
+        set => _eventName = value;
+    }
+
+    public TextMeshProUGUI EventDescription
+    {
+        get => _eventDescription;
+        set => _eventDescription = value;
+    }
+
+    public TextMeshProUGUI EventOption1
+    {
+        get => _eventOption1;
+        set => _eventOption1 = value;
+    }
+
+    public TextMeshProUGUI EventOption2
+    {
+        get => _eventOption2;
+        set => _eventOption2 = value;
+    }
+
+    public TextMeshProUGUI EventEffect1
+    {
+        get => _eventEffect1;
+        set => _eventEffect1 = value;
+    }
+
+    public TextMeshProUGUI EventEffect2
+    {
+        get => _eventEffect2;
+        set => _eventEffect2 = value;
     }
 }
--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -13,14 +13,28 @@ public class Events : MonoBehaviour
 
     [SerializeField] private GameObject _windowClicker;
     [SerializeField] private GameObject _windowEvent;
+
     [SerializeField] private GuiSfx _guiSfx;
+
+    [SerializeField] private GuiData _guiData;
 
     private bool _hasEvent;
 
     private int _randomSeconds;
+    private int _randomEvent;
+
+    private List<Event> _eventList;
     private void Awake()
     {
+        _eventList = new List<Event>
+        {
+            new Event("ExampleName1", "ExampleDescription1", "ExampleOptionA1", "ExampleOptionB1", "ExampleEffectsA1", "ExampleEffectsB1"),
+            new Event("ExampleName2", "ExampleDescription2", "ExampleOptionA2", "ExampleOptionB2", "ExampleEffectsA2", "ExampleEffectsB2"),
+            new Event("ExampleName3", "ExampleDescription3", "ExampleOptionA3", "ExampleOptionB3", "ExampleEffectsA3", "ExampleEffectsB3"),
+        };
+
         _randomSeconds = Random.Range(_minSeconds, _maxSeconds);
+        _randomEvent = Random.Range(0, _eventList.Count);
         Invoke("PushEvent", _randomSeconds);
     }
     
@@ -30,6 +44,7 @@ public class Events : MonoBehaviour
         _hasEvent = true;
 
         _randomSeconds = Random.Range(_minSeconds, _maxSeconds);
+        _randomEvent = Random.Range(0, _eventList.Count);
         Invoke("PushEvent", _randomSeconds);
     }
 
@@ -40,9 +55,37 @@ public class Events : MonoBehaviour
             _eMark.color = new Color(0, 230, 0);
 
             _guiSfx.OnButtonClick();
+
+            _guiData.EventName.text = _eventList[_randomEvent].Name;
+            _guiData.EventDescription.text = _eventList[_randomEvent].Description;
+            _guiData.EventOption1.text = _eventList[_randomEvent].Option1;
+            _guiData.EventOption2.text = _eventList[_randomEvent].Option2;
+            _guiData.EventEffect1.text = _eventList[_randomEvent].Effect1;
+            _guiData.EventEffect2.text = _eventList[_randomEvent].Effect2;
+
             _hasEvent = false;
             _windowClicker.SetActive(false);
             _windowEvent.SetActive(true);
+        }
+    }
+
+    class Event
+    {
+        public string Name;
+        public string Description;
+        public string Option1;
+        public string Option2;
+        public string Effect1;
+        public string Effect2;
+
+        public Event(string name, string description, string option1, string option2, string effect1, string effect2)
+        {
+            Name = name;
+            Description = description;
+            Option1 = option1;
+            Option2 = option2;
+            Effect1 = effect1;
+            Effect2 = effect2;
         }
     }
 }
